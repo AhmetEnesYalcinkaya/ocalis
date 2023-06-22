a5d923e8c73495c51748c90071a7bd69e19c69d3
Ahmet Enes Yalcinkaya <39588954+AhmetEnesYalcinkaya@users.noreply.github.com>
2023-06-22 13:30:18 +0300

    Update setup.sh

--- setup.sh
+++ setup.sh
@@ -1,9 +1,9 @@
 mkdir -p ~/.streamlit/
-echo "
-[server]\n
-headless=true\n
-enableCORS=false\n
-enableXsrfProtection=false\n
-port=8080\n
-\n
+
+echo "\
+[server]\n\
+port = $PORT\n\
+enableCORS = false\n\
+headless = true\n\
+\n\
 " > ~/.streamlit/config.toml
